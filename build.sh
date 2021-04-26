cfd31ba2d078d8b9e5ebb345b613878b687aef38
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2021-04-26 15:55:16 +0200

    Update builds

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 # https://golang.org/doc/install/source#environment
 
 # Linux
-env GOOS=linux GOARCH=arm go build -o build/cli-linux -v cli.go  
+env GOOS=linux GOARCH=arm go build -o build/cli-linux-arm -v cli.go  
+env GOOS=linux GOARCH=386 go build -o build/cli-linux-386 -v cli.go  
+env GOOS=linux GOARCH=386 go build -o build/cli-linux-amd64 -v cli.go  
 
 # macos
 env GOOS=darwin GOARCH=amd64 go build -o build/cli-darwin -v cli.go 
